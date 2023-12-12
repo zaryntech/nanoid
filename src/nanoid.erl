ba450219a55e5baac34b38668a7843582d0f975b
emilano <arvandazadvar@proton.me>
2023-12-12 11:49:03 +0330

    fixing

--- src/nanoid.erl
+++ src/nanoid.erl
@@ -1,10 +1,9 @@
 -module(nanoid).
+-author("Zaryn Technologies").
 -export([gen/0]).
-
 -include_lib("kernel/include/logger.hrl").
-
 -import(nanoid_config, [generate/0]).
 
-
+%% Main function to generate random key 
 gen() ->
     erlang:binary_to_list(nanoid_config:generate()).
--- src/nanoid_config.erl
+++ src/nanoid_config.erl
@@ -1,24 +1,27 @@
 -module(nanoid_config).
-
+-author("Zaryn Technologies").
 -export([generate/0, generate/2]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
+%% @doc Generates a random string using the specified alphabet and size.
 -spec generate() -> bitstring().
-
 generate() ->
-    generate(<<"_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ">>, 21).
-
+    generate(
+    <<"_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$%@*^>][<?}{!">>,
+    61).
 
 -spec generate(bitstring(), integer()) -> bitstring().
-
 generate(Alphabet, Size) when Size > 0 ->
     AlpLen = byte_size(Alphabet),
     Mask = calc_mask(AlpLen),
     Step = calc_step(Mask, Size, AlpLen),
-    generate_nanoid(Alphabet, Size, Mask, Step, <<"">>).
+    generate_nanoid(Alphabet, Size, Mask, Step, <<"">>);
+
+generate(_, _) ->
+    {error, invalid_size}.
 
 %%====================================================================
 %% Internal functions
@@ -32,7 +35,6 @@ generate_nanoid(Alphabet, Size, Mask, Step, Acc) ->
     Nanoid = random_string(Alphabet, Random, Mask, Step, Acc),
     generate_nanoid(Alphabet, Size, Mask, Step, Nanoid).
 
-
 random_string(_Alphabet, _Random, _Mask, 0, Acc) ->
     Acc;
 
@@ -45,10 +47,8 @@ random_string(Alphabet, Random, Mask, Step, Acc) ->
              end,
     random_string(Alphabet, Random, Mask, Step - 1, <<Acc/binary, Append/binary>>).
 
-
 calc_mask(AlpLen) ->
     (2 bsl round(math:floor(math:log(AlpLen - 1)/math:log(2)))) - 1.
-
 
 calc_step(Mask, Size, AlpLen) ->
     Step = math:ceil(1.6 * Mask * Size / AlpLen),
